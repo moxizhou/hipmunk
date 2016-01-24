d783954b3feb0d0ed82122157ce002a1f808e421
Moxi Zhou <moxi@Moxis-MacBook-Pro.local>
2016-01-24 00:46:26 -0800

    fix up some stuff

--- tictactoe.js
+++ tictactoe.js
@@ -1,142 +1,126 @@
-var PLAYER_MOVE = 'X';
-var AI_MOVE = 'O';
 var ttt;
 
 function playerClick() {
-	var row = $(this).attr('value');
+	var index = $(this).attr('value');
 
 	//if player's turn and valid move
-	if (ttt.move(row, ttt.player)) {
-		//add player's move
-		$(this).addClass('fa fa-times fa-4x');
+	if (ttt.move(index, ttt.player, ttt.board)) {
+		UIManager.setMove($(this), true);
 
-		//calculate and add ai's move
-		var bestMove = ai.move(ttt.board);
-		$('td[value=' + bestMove + ']').addClass('fa fa-circle-o fa-4x');
-		
+		var aiMove = ai.move(ttt.board);
+		UIManager.setMove($('td[value=' + aiMove + ']'));		
 		checkWinner();
 	}
 }
 
 function checkWinner() {
-	var winner = ttt.getScore();
+	var winner = ttt.getScore(ttt.board);
 	var text;
 	if (winner !== false) {
-		switch(winner) {
-			case 1:
-				text = 'You lost!'
-				break;
-			case -1:
-				text = 'You won!'
-				break;
-			case 0:
-				text = 'You tied!'
-				break;
-			default:
-				break;
+		if (winner === 1) {
+			text = 'You lost!';
+		} else if (winner === -1) {
+			text = 'You won!';
+		} else {
+			text = 'You tied!';
 		}
-		//fade board and turn click event off
-		$('.board').fadeTo(500, 0.3);
-    	$('.overlay').css('display', 'block').addClass('replay');
-		//display win/lose/tie results in center and allow click of retry
-		$('.end').text(text);
-		$('.fa-repeat').on('click', initialize)
-			.parent().css('display', 'flex');
+		UIManager.toggleEndGame(text);
 	}
-	
-}
-
-function initialize() {
-	$('.board').css('display', 'none');
-	$('.initial').css('display', 'block');
-	$('.player-start').on('click', start);
-	$('.ai-start').on('click', true, start);
-	$('.overlay').click(function noop(e) { return e.preventDefault(); });
 }
 
 function start(event) {
 	//instantiate class
 	ttt = new TicTacToe();
-
-	$('.overlay').css('display', 'none');
-	$('.board').fadeTo(500, 1);
+	UIManager.toggleBoard();
 	$('td').removeClass().on('click', playerClick);
 	if (event.data) {
 		var bestMove = ai.move(ttt.board);
-		$('td[value=' + bestMove + ']').addClass('fa fa-circle-o fa-4x');
+		UIManager.setMove($('td[value=' + bestMove + ']'));
 	}
 }
 
-initialize();
+var UIManager = {
+	setMove: function(box, isPlayer) {
+		var icon = isPlayer ? 'fa-times' : 'fa-circle-o'; 
+		box.addClass('fa fa-4x ' + icon);
+	},
+	toggleEndGame: function(endText) {
+		$('.board').fadeTo(500, 0.3);
+    	$('.overlay').css('display', 'block').addClass('replay');
+		//display win/lose/tie results in center and allow click of retry
+		$('.end').text(endText);
+		$('.fa-repeat').on('click', this.initialize)
+			.parent().css('display', 'flex');
+	},
+	initialize: function() {
+		$('.board').css('display', 'none');
+		$('.initial').css('display', 'block');
+		$('.player-start').on('click', start);
+		$('.ai-start').on('click', true, start);
+		$('.overlay').click(function noop(e) { return e.preventDefault(); });
+	},
+	toggleBoard: function() {
+		$('.overlay').css('display', 'none');
+		$('.board').fadeTo(500, 1);
+	}
+}
+UIManager.initialize();
 
 function TicTacToe() {
 	this.board = new Array(9);
-	this.player = PLAYER_MOVE;
-	this.ai = AI_MOVE;
+	this.player = 'X';
+	this.ai = 'O';
 }
 
-function move(i, player, board) {
-	board = board || this.board;
+TicTacToe.prototype.move = function(i, player, board) {
 	if (!board[i]) {
 		board[i] = player;
 		return true;
 	}
 	return false;
-}
+};
 
-TicTacToe.prototype.move = move;
-
-function isHorizontalVictory(board, player) {
-	return (board[0] == player && board[1] == player && board[2] == player) ||
-		(board[3] == player && board[4] == player && board[5] == player) ||
-		(board[6] == player && board[7] == player && board[8] == player);
-} 
-function isVerticalVictory(board, player) {
-	return (board[0] == player && board[3] == player && board[6] == player) ||
-		(board[1] == player && board[4] == player && board[7] == player) ||
-		(board[2] == player && board[5] == player && board[8] == player);
-}
-function isDiagonalVictory(board, player) {
-	return (board[0] == player && board[4] == player && board[8] == player) ||
-		(board[2] == player && board[4] == player && board[6] == player);
-}
-TicTacToe.prototype.hasWinner = function(player, board) {
-	board = board || this.board;
-	return isHorizontalVictory(board, player) || isVerticalVictory(board, player) || isDiagonalVictory(board, player);
-}
+TicTacToe.prototype._hasWinner = function(player, board) {
 	
-
-TicTacToe.prototype.noMoves = function(board) {
-	board = board || this.board;
-	// return _.contains(board, '');
-	var result = true;
-	for (var i = 0; i < board.length; i++) {
-		if (!board[i]) {
-			result = false;
-			break;
-		}
+	function isHorizontalVictory(board, player) {
+		return (board[0] === player && board[1] === player && board[2] === player) ||
+			(board[3] === player && board[4] === player && board[5] === player) ||
+			(board[6] === player && board[7] === player && board[8] === player);
+	} 
+	function isVerticalVictory(board, player) {
+		return (board[0] === player && board[3] === player && board[6] === player) ||
+			(board[1] === player && board[4] === player && board[7] === player) ||
+			(board[2] === player && board[5] === player && board[8] === player);
 	}
-	return result;
-}
+	function isDiagonalVictory(board, player) {
+		return (board[0] === player && board[4] === player && board[8] === player) ||
+			(board[2] === player && board[4] === player && board[6] === player);
+	}
 
-//all the conditions to win 
+	return isHorizontalVictory(board, player) || isVerticalVictory(board, player) || isDiagonalVictory(board, player);
+};
+	
+TicTacToe.prototype._noMoves = function(board) {
+	return !_.contains(board, undefined);
+};
+
+//All the conditions to win 
 TicTacToe.prototype.getScore = function(board) {
-	board = board || this.board;
-	if (this.hasWinner(this.ai, board)) {
+	if (this._hasWinner(this.ai, board)) {
 		return 1;
-	} else if (this.hasWinner(this.player, board)) {
+	} else if (this._hasWinner(this.player, board)) {
 		return -1;
-	} else if (this.noMoves(board)) {
+	} else if (this._noMoves(board)) {
 		return 0;
 	} else {
 		return false;
 	}
-}
+};
 
 function onNextMove(board, player, cb) {
 	for (var i = 0; i < board.length; i++) {
 		var newBoard = _.clone(board);
-		var nextMove = move(i, player, newBoard);
+		var nextMove = ttt.move(i, player, newBoard);
 		if (nextMove) cb(newBoard, i);
 	}
 }
@@ -144,7 +128,7 @@ function onNextMove(board, player, cb) {
 var ai = {
 	move: function(board) {
 		var bestMove = this._minimax(board);
-		ttt.move(bestMove, AI_MOVE);
+		ttt.move(bestMove, ttt.ai, ttt.board);
 		return bestMove;
 	},
 	//minimax recursive decision tree, always assume opponent is the highest level
@@ -152,7 +136,7 @@ var ai = {
 		var topScore = this._ARBITRARY_LOW_NUMBER;
 		var move = 0;
 		
-		onNextMove(board, AI_MOVE, function(newBoard, _move) {
+		onNextMove(board, ttt.ai, function(newBoard, _move) {
 			var currentScore = ai._minValue(newBoard);
 			if (currentScore > topScore) {
 				topScore = currentScore;
@@ -168,7 +152,7 @@ var ai = {
 			return winner;
 		}
 		var topScore = this._ARBITRARY_HIGH_NUMBER;
-		onNextMove(board, PLAYER_MOVE, function(newBoard) {
+		onNextMove(board, ttt.player, function(newBoard) {
 			var currentScore = ai._maxValue(newBoard);
 			if (currentScore < topScore) {
 				topScore = currentScore;
@@ -183,7 +167,7 @@ var ai = {
 			return winner;
 		}
 		var topScore = this._ARBITRARY_LOW_NUMBER;
-		onNextMove(board, AI_MOVE, function(newBoard) {
+		onNextMove(board, ttt.ai, function(newBoard) {
 			var currentScore = ai._minValue(newBoard);
 			if (currentScore > topScore) {
 				topScore = currentScore;
